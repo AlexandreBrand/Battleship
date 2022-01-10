0482f6a4d94ec7be0bbacd0710468574ecc79571
Laurent <lwieser@capcod.eu>
2022-01-10 13:47:06 +0100

    init

---
+++ Battleship.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Battleship.Tests
+{
+    [TestClass]
+    public class ProgramTester
+    {
+        [TestMethod]
+        public void Runs()
+        {
+            Program.Main(Array.Empty<string>());
+        }
+    }
+}
