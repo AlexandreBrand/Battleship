eee153170277c09fa883b1d69ef2f627f198db0e
Laurent <lwieser@capcod.eu>
2022-01-10 14:18:08 +0100

    Build fails beacause Battleshipprograme does not take communicator

--- Battleship.Logic.Tests/Interfaces/UnitTest1.cs
+++ Battleship.Logic.Tests/Interfaces/UnitTest1.cs
@@ -1,13 +1,28 @@
+using Battleship.Logic.Interfaces;
+using Battleship.Logic.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Battleship.Logic.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class BattleshipProgramTester
     {
-        [TestMethod]
-        public void TestMethod1()
+        private BattleshipProgram _program;
+        private Mock<ICommunicator> _commMock;
+
+
+        public BattleshipProgramTester()
         {
+            _commMock = new Mock<ICommunicator>();
+            _program = new BattleshipProgram(_commMock.Object);
+        }
+
+        [TestMethod]
+        public void Runs_ThenCommunicatorisCalled()
+        {
+            _program.Run();
+
         }
     }
 }
